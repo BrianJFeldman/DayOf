df5e5479709d2955f40cec59db94af093b494d9c
Brian Feldman <brianfeldman@Caitlins-MacBook-Pro.local>
2017-04-28 11:09:15 -0700

    042817 push

--- dayOf.js
+++ dayOf.js
@@ -4,14 +4,11 @@ function gText(e) {
     //stored selected text in date variable
     dateCheck = (document.all) ? document.selection.createRange().text.toString() : document.getSelection().toString();
     // covert to usable date string
-    console.log(dateCheck)
     if (dateCheck.includes('nd')) dateCheck = dateCheck.replace('nd', '');
     if (dateCheck.includes('rd')) dateCheck = dateCheck.replace('rd', '');
     if (dateCheck.includes('th')) dateCheck = dateCheck.replace('th', '');
     if (dateCheck.includes('1st')) dateCheck = dateCheck.replace('1st', '1');
-    console.log(dateCheck)
     output = new Date(dateCheck)
-    console.log(output)
     if (output != 'Invalid Date') {
         output = output.toISOString();
         output = output.substring(0, 10);
@@ -19,21 +16,19 @@ function gText(e) {
             output = output.replace('2001', new Date().getFullYear());
         }
         output = output.replace(/-/g, '');
-        console.log('output')
         let url = 'https://calendar.google.com/calendar/render?tab=mc&date=' +
             output + '&mode=day'
-        function wrapSelectedText() {
-            console.log('wrap is running')
+        function linkify() {
             var selection = window.getSelection().getRangeAt(0);
             var selectedText = selection.extractContents();
             var a = document.createElement("a");
-            a.style.fontDecoration = "none";
+            a.style.cssText= "text-decoration:none; font-weight:bolder; color:#f48c42";
+            a.classList.add('dayOffLink')
             a.appendChild(selectedText);
             a.setAttribute('href', `${url}`)
             selection.insertNode(a);
-            console.log('hi')
         }
-        wrapSelectedText();
+        linkify();
     }
 }
 
